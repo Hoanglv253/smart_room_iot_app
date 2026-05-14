b0370cd1d8334760f69503e444fea45f3f22087b
Long Van Hoang <lvhoang966@gmail.com>
2026-05-15 06:54:06 +0700

    chore: setup Firebase core va khoi tao trong main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart'; // Import file màn hình đăng nhập của bạn
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const SmartRoomApp());
+void main() async {
+  // 1. Lệnh bắt buộc để Flutter có thể giao tiếp với hệ thống (Native) trước khi vẽ giao diện
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. Khởi động Firebase và nạp file cấu hình mà em vừa tạo ban nãy
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // 3. Chạy App lên
+  runApp(
+    const SmartRoomApp(),
+  ); // Lưu ý: Nếu App gốc của em tên khác thì giữ nguyên tên đó nhé
 }
 
 class SmartRoomApp extends StatelessWidget {
